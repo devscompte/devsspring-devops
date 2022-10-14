09e27c4513b3c69e02291f793c8bb87a58b59788
atouts20 <60098245+atouts20@users.noreply.github.com>
2022-10-14 23:01:09 +0100

    first commit

---
+++ src/main/java/org/devsspring/DevsspringdevopsApplication.java
@@ -0,0 +1,13 @@
+package org.devsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsspringdevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsspringdevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/devsspring/DevsspringdevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsspringdevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
